0d9bf8f26ecbf4ee3ce92e925851a2baf155bab7
Roberto C. Hernandez <carlosh95@gmail.com>
2016-04-22 15:50:36 -0400

    refactor main function

--- src/index.js
+++ src/index.js
@@ -7,42 +7,45 @@ var gutil = require('gulp-util');
 var lazypipe = require('lazypipe');
 var plugins = require('gulp-load-plugins')({ lazy: true });
 
-module.exports = validatePipeline;
+var cssFilter = gulpFilter('*.css', { matchBase: true, restore: true });
 
-function validatePipeline() {
-
-  var pipeline = {
-    validateCSS: validateCSS()
-  };
-
-  var cssFilter = gulpFilter('*.css', { matchBase: true, restore: true });
-
-  return pipeline;
-
-  function validateCSS() {
-    gutil.log('Validating CSS files.');
-    return lazypipe()
-      .pipe(function() {
-        return cssFilter;
-      })
-      .pipe(plugins.csslint)
-      .pipe(plugins.csslint.reporter)
-      .pipe(plugins.csslint.reporter, customReporter)
-      .pipe(plugins.csslint.failReporter)
-      .pipe(function() {
-        gutil.log('Restoring CSS Filter.');
-        return cssFilter.restore;
-      });
+module.exports = {
+  validateCSS: function() {
+    return validateCSS();
   }
+};
 
-  function customReporter(file) {
-    var color = gutil.colors;
 
-    gutil.log(color.red('Errors in: ' + file.path));
-    file.csslint.results.forEach(function(result) {
-      gutil.log(color.grey('line ' + result.error.line + ':' + result.error.message));
+function makePipe() {
+  gutil.log('Validating CSS files.');
+  var stream =  lazypipe()
+    .pipe(function() {
+      return cssFilter;
+    })
+    .pipe(plugins.csslint)
+    .pipe(plugins.csslint.reporter)
+    .pipe(plugins.csslint.reporter, customReporter)
+    .pipe(plugins.csslint.failReporter)
+    .pipe(function() {
+      gutil.log('Restoring CSS Filter.');
+      return cssFilter.restore;
     });
-    gutil.log(color.red(' -- End Errors -- '));
-  }
+
+  return stream();
 }
 
+function customReporter(file) {
+  var color = gutil.colors;
+
+  gutil.log(color.red('Errors in: ' + file.path));
+  file.csslint.results.forEach(function(result) {
+    gutil.log(color.grey('line ' + result.error.line + ':' + result.error.message));
+  });
+  gutil.log(color.red(' -- End Errors -- '));
+}
+
+function validateCSS(){
+  var pipeline = makePipe();
+
+  return pipeline;
+}
